1e8b2e4328535823d93ee63a707532b024a30b2d
Khalimov-Z <djordlo077@gmail.com>
2021-04-04 21:45:32 +0300

    Настроил главный файл проекта - index.jsx:
    - Убрал лишний мусор
    - Импортировал стили bootstrap
    - Подключил провайдер и прокинул объект store
    - Добавил .jsx расширение

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from 'react-redux'
 import { store } from './redux'
 
 ReactDOM.render(
-
   <React.StrictMode>
     <Provider store={store}>
       <App />
