07fd08c90e1b32bb692518a6c0f61c96285d47ad
Khalimov-Z <djordlo077@gmail.com>
2021-04-08 22:55:37 +0300

    Сделал изменений

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
-import application from "./application";
 import products from "./products";
 import recipes from "./recipes";
 
 export const rootReducer = combineReducers({
-  application,
   products,
   recipes,
 });
