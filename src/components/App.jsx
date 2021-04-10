2cf26ac55b78f22b9f6e7f2ab018fc4ecc9fb4f4
shishaniyah <arsaevaaisha@gmail>
2021-04-10 21:55:18 +0300

    Добавила компонент Main.jsx в App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,18 @@
+import React, { useEffect } from 'react'
+import Main from './Main'
+import { useDispatch } from 'react-redux'
+import { loadRecipes } from '../redux/reducers/recipes'
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRecipes());
+  }, [dispatch]);
+
   return (
     <div className="App">
-
+      <Main />
     </div>
   );
 }
