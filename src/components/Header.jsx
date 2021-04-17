87a6ac8976ea96301b88b11c1848ef97a7734a60
Khalimov-Z <djordlo077@gmail.com>
2021-04-17 12:30:14 +0300

    Сделал выпадающий список в Header.jsx категории рецептов

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { FiBookmark } from 'react-icons/all'
 
 function Header() {
   return (
     <div className="header">
-      <div className="container">
-        <div className="row">
-          <div className="col-4">
+      <div className="container-lg">
+        <div className="row  justify-content-between">
+          <div className="col-3">
             <div className="logo">
               <img
                 src="https://im0-tub-ru.yandex.net/i?id=77b7ea8c917a0bde08b581414328f261&n=13"
@@ -14,26 +15,36 @@ function Header() {
               />
             </div>
           </div>
-          <div className="col">
-            <div className="kategori">Котегории</div>
-          </div>
-          <div className="col-1">
-            <div className="oNas">О нас</div>
-          </div>
-          <div className="col-1">
-            <div className="oNas">Контакты</div>
-          </div>
-          <div className="col-2">
-            <div className="bookmark">
-              <img
-                src="https://www.flaticon.com/svg/vstatic/svg/786/786251.svg?token=exp=1617880331~hmac=634d832f199bd07d4d1e3fb4ced98b75"
-                alt="bookmarkImg"
-              />
-              <div className="schetchick">1</div>
-            </div>
-          </div>
-          <div className="col-1">
-            <button className="Login">Войти</button>
+          <div className="col-8">
+            <ul className="nav d-flex justify-content-between">
+              <li className="nav-item">
+                <select className="kategori">
+                  <option selected>Кaтегории рецептов</option>
+                  <option>Все</option>
+                  <option>Десерты</option>
+                  <option>Мясные</option>
+                  <option>Напитки</option>
+                  <option>Супы</option>
+                </select>
+              </li>
+              <li className="nav-item">
+                <div className="oNas">О нас</div>
+              </li>
+              <li className="nav-item">
+                <div className="oNas">Контакты</div>
+              </li>
+              <li className="nav-item">
+                 <FiBookmark
+                   size={25}
+                   style={{marginTop:22}}
+
+                 />
+                  <div className="schetchick">1</div>
+              </li>
+              <li className="nav-item">
+                <button className="Login">Войти</button>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
