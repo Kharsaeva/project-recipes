36d3b855b7feea6d02fbe709fa090abd7d8b8abe
shishaniyah <arsaevaaisha@gmail>
2021-04-18 20:58:09 +0300

    Доработала стили в Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { FaUserCircle, FaBookmark } from "react-icons/all";
 
 function Header() {
   return (
-    <div className="header">
+    <div className="header m-auto">
       <div className="container">
-        <div className="row">
+        <div className="row justify-content-between">
           <div className="col-4">
             <div className="logo">
               <img
@@ -14,26 +15,18 @@ function Header() {
               />
             </div>
           </div>
-          <div className="col">
-            <div className="kategori">Котегории</div>
-          </div>
-          <div className="col-1">
-            <div className="oNas">О нас</div>
-          </div>
-          <div className="col-1">
-            <div className="oNas">Контакты</div>
-          </div>
-          <div className="col-2">
-            <div className="bookmark">
-              <img
-                src="https://www.flaticon.com/svg/vstatic/svg/786/786251.svg?token=exp=1617880331~hmac=634d832f199bd07d4d1e3fb4ced98b75"
-                alt="bookmarkImg"
-              />
-              <div className="schetchick">1</div>
-            </div>
-          </div>
-          <div className="col-1">
-            <button className="Login">Войти</button>
+          <div className="col-auto">
+            <ul>
+              <li>Категории</li>
+              <li>О нас</li>
+              <li>Контакты</li>
+              <li>
+                <FaBookmark />
+              </li>
+              <li>
+                <FaUserCircle />
+              </li>
+            </ul>
           </div>
         </div>
       </div>
