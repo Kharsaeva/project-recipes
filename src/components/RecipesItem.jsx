4e110c37264015edad3c88c623af426610a670d8
shishaniyah <arsaevaaisha@gmail>
2021-04-18 22:41:46 +0300

    Удалила RecipesItem.jsx из Search.jsx и добавила в компонент Recipes.jsx

--- src/components/RecipesItem.jsx
+++ src/components/RecipesItem.jsx
@@ -4,13 +4,14 @@ import React, { useState } from "react";
 import { FiThumbsUp, FiBookmark, FiMessageCircle } from "react-icons/fi";
 import { favoritePatch } from "../redux/reducers/recipes";
 import ModalW from "./ModalW";
+import { IoAlarm, IoFastFood } from "react-icons/all";
 
 function RecipesItem(props) {
   const dispatch = useDispatch();
+
   const itemDeleting = (id) => {
     dispatch(itemDelete(id));
   };
-
 
   const [modalActive, setModalActive] = useState(false);
 
@@ -32,8 +33,8 @@ function RecipesItem(props) {
   };
 
   return (
-    <div className="RecipesItem m-auto d-block">
-      <div className="pb-4">
+    <div className="recipes-item m-auto d-block">
+      <div>
         <div className="d-inline d-flex pb-4 justify-content-between">
           <img
             className="mr-4"
@@ -41,63 +42,71 @@ function RecipesItem(props) {
             style={{ width: "50%" }}
             alt="img"
           />
-          <h2 className="align-self-center">{props.item.title}</h2>
+          <h3 className="align-self-center">{props.item.title}</h3>
           <button className="exit" onClick={() => itemDeleting(props.item.id)}>
             ❌
           </button>
         </div>
         <div className="d-block align-self-center pb-4">
-          <span className="mr-5">Каллорийность: {props.item.calories}</span>
-          <span>Время приготовления: {props.item.time}</span>
+          <IoFastFood />
+          <span className="mr-5"> : {props.item.calories}</span>
+          <IoAlarm />
+          <span> : {props.item.time}</span>
         </div>
-        <h5>Ингредиенты:</h5>
-        <span>{props.item.ingredients}</span>
       </div>
-      <div className="cooking-method">
-        <h5>Способ приготовления:</h5>
-        <div>{props.item.cooking}</div>
-      </div>
-      <div className="d-inline d-flex w-auto p-4">
-        <div className="LikeOne">
-          <FiThumbsUp
-            size={25}
-            fill={likeStatus ? "black" : "none"}
-            onClick={() => likeActive()}
-          />{" "}
-          {likeClick}
-        </div>
-        <div
-          style={{ borderRight: "1px solid darkgrey" }}
-          className="ml-4 mr-4"
-        >
-          {" "}
-        </div>
-        <div>
-          <FiBookmark
-            size={25}
-            fill={props.item.favorite ? "black" : "none"}
-            onClick={() => setFavorite(props.item.id, props.item.favorite)}
-          />
 
+      <div className={``}>
+        <div className="pb-4">
+          <h5>Ингредиенты:</h5>
+          <div>{props.item.ingredients}</div>
         </div>
-        <div
-          style={{ borderRight: "1px solid darkgrey" }}
-          className="ml-4 mr-4"
-        >
-          {" "}
+        <div className="cooking-method">
+          <h5>Способ приготовления:</h5>
+          <div>{props.item.cooking}</div>
         </div>
-        <div className="LikeOne" onClick={() => commOpen()}>
-          <FiMessageCircle size={25} />
+        <div className="d-inline d-flex w-auto p-4">
+          <div className="LikeOne">
+            <FiThumbsUp
+              size={25}
+              fill={likeStatus ? "black" : "none"}
+              onClick={() => likeActive()}
+            />{" "}
+            {likeClick}
+          </div>
+          <div
+            style={{ borderRight: "1px solid darkgrey" }}
+            className="ml-4 mr-4"
+          >
+            {" "}
+          </div>
+          <div>
+            <FiBookmark
+              size={25}
+              fill={props.item.favorite ? "black" : "none"}
+              onClick={() => setFavorite(props.item.id, props.item.favorite)}
+            />
+          </div>
+          <div
+            style={{ borderRight: "1px solid darkgrey" }}
+            className="ml-4 mr-4"
+          >
+            {" "}
+          </div>
+          <div className="LikeOne" onClick={() => commOpen()}>
+            <FiMessageCircle size={25} />
+          </div>
         </div>
+
+        <ModalW
+          active={modalActive}
+          setActive={setModalActive}
+          item={props.item}
+        />
       </div>
+
       <div style={{ borderTop: "1px solid darkgrey" }} className="pb-4">
         {" "}
       </div>
-      <ModalW
-        active={modalActive}
-        setActive={setModalActive}
-        item={props.item}
-      />
     </div>
   );
 }
